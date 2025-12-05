4a34e1bf8485a9c9ebf71f26d3266e8aa74e426d
Frederik Finckh <frederik.finckh@gmx.de>
2025-12-05 23:34:01 +0100

    use correct url again for building

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SPOTIFY_CONFIG = {
     clientId: '4dbb9549f524420aa61327c3ca9b0220',
-    // redirectUri: 'https://cirrocumulus.org/login.html',
-    redirectUri: 'http://127.0.0.1:5173/login.html',
+    redirectUri: 'https://cirrocumulus.org/login.html',
+    // redirectUri: 'http://127.0.0.1:5173/login.html',
     scopes: [
         'user-read-private',
         'user-read-email',
